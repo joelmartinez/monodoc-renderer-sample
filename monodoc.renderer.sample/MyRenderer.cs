837d9d9a96598ee7ad4f8676eab2dd64e6e2808c
Joel Martinez <joelmartinez@gmail.com>
2016-06-23 15:44:49 -0400

    renderer stub added

--- monodoc.renderer.sample/MyRenderer.cs
+++ monodoc.renderer.sample/MyRenderer.cs
@@ -1,10 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
+using Monodoc;
+
 namespace monodoc.renderer.sample
 {
-	public class MyRenderer
+	public class MyRenderer : IDocGenerator<string>
 	{
 		public MyRenderer()
 		{
+		}
+
+		public string Generate(HelpSource hs, string internalId, Dictionary<string, string> context)
+		{
+			throw new NotImplementedException();
 		}
 	}
 }
